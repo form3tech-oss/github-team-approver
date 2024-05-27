f071a21a606be125cf7e043baef2adb7082a2d03
przechrzta-f3 <wojciech.przechrzta@form3.tech>
2024-05-27 15:57:13 +0200

    feat: Update pact tests

--- internal/api/handler_test.go
+++ internal/api/handler_test.go
@@ -76,44 +76,44 @@ func Test_Handle(t *testing.T) {
 
 			expectedFinalStatus: approval.StatusEventStatusSuccess,
 		},
-		//{
-		//	name: `PR review Submitted (requires approval from the "CAB" and "Documentation" teams)`,
-		//
-		//	eventType:      eventTypePullRequestReview,
-		//	eventBody:      readGitHubExampleFile("pull_request_review_submitted.json"),
-		//	eventSignature: "sha256=802a01c378001fbbbea8f59e7d5eab688550bcbd097491abc907d8850cef6e17",
-		//	pacts: []pacttesting.Pact{
-		//		"pull_request_review_submitted_pending",
-		//		"issue_events_pr_7",
-		//	},
-		//
-		//	expectedFinalStatus: approval.StatusEventStatusPending,
-		//},
-		//{
-		//	name: `PR review Submitted (requires approval from the "CAB" and "Documentation" teams)`,
-		//
-		//	eventType:      eventTypePullRequestReview,
-		//	eventBody:      readGitHubExampleFile("pull_request_review_submitted_force_approval.json"),
-		//	eventSignature: "sha256=c4d9e2a311de0322c4b7c09c1a2239d23668542c9caf187be03c7acb62f3ca5b",
-		//	pacts: []pacttesting.Pact{
-		//		"pull_request_review_submitted_force_approval",
-		//		"issue_events_pr_7",
-		//	},
-		//
-		//	expectedFinalStatus: approval.StatusEventStatusSuccess,
-		//},
-		//{
-		//	name: `PR review Submitted (no regular expressions matched)`,
-		//
-		//	eventType:      eventTypePullRequestReview,
-		//	eventBody:      readGitHubExampleFile("pull_request_review_submitted_no_regexes_matched.json"),
-		//	eventSignature: "sha256=9b5e234c6deff549b631d7e08363e9e90e0bdf635e3a440e2b40cef5fab3205a",
-		//	pacts: []pacttesting.Pact{
-		//		"pull_request_review_submitted_no_regexes_matched",
-		//	},
-		//
-		//	expectedFinalStatus: approval.StatusEventStatusPending,
-		//},
+		{
+			name: `PR review Submitted (requires approval from the "CAB" and "Documentation" teams)`,
+
+			eventType:      eventTypePullRequestReview,
+			eventBody:      readGitHubExampleFile("pull_request_review_submitted.json"),
+			eventSignature: "sha256=802a01c378001fbbbea8f59e7d5eab688550bcbd097491abc907d8850cef6e17",
+			pacts: []pacttesting.Pact{
+				"pull_request_review_submitted_pending",
+				"issue_events_pr_7",
+			},
+
+			expectedFinalStatus: approval.StatusEventStatusPending,
+		},
+		{
+			name: `PR review Submitted (requires approval from the "CAB" and "Documentation" teams)`,
+
+			eventType:      eventTypePullRequestReview,
+			eventBody:      readGitHubExampleFile("pull_request_review_submitted_force_approval.json"),
+			eventSignature: "sha256=c4d9e2a311de0322c4b7c09c1a2239d23668542c9caf187be03c7acb62f3ca5b",
+			pacts: []pacttesting.Pact{
+				"pull_request_review_submitted_force_approval",
+				"issue_events_pr_7",
+			},
+
+			expectedFinalStatus: approval.StatusEventStatusSuccess,
+		},
+		{
+			name: `PR review Submitted (no regular expressions matched)`,
+
+			eventType:      eventTypePullRequestReview,
+			eventBody:      readGitHubExampleFile("pull_request_review_submitted_no_regexes_matched.json"),
+			eventSignature: "sha256=9b5e234c6deff549b631d7e08363e9e90e0bdf635e3a440e2b40cef5fab3205a",
+			pacts: []pacttesting.Pact{
+				"pull_request_review_submitted_no_regexes_matched",
+			},
+
+			expectedFinalStatus: approval.StatusEventStatusPending,
+		},
 		//{
 		//	name: `PR review Submitted (requires approval from at least one of the "CAB - Foo" and "CAB - BAR" teams, as well as from the "CAB - Documentation" team)`,
 		//
